fb8b4138a4b3af68b6a7e4cf9f18575a1e7549d0
steven20012500 <steven20012500@gmail.com>
2024-07-15 13:26:48 -0500

    atlas

--- servidor-backend/database.js
+++ servidor-backend/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb+srv://jsuntaxic:nyAxUz78d0sHNLAp@Restaurante.kdgk98n.mongodb.net/restaurante?retryWrites=true&w=majority'
+const URI = 'mongodb+srv://jsuntaxic:nyAxUz78d0sHNLAp@restaurante.kdgk98n.mongodb.net/?retryWrites=true&w=majority&appName=Restaurante'
 mongoose.connect(URI)
 .then (db => console.log('DB conectada'))
 .catch(err => console.error(err));
 module.exports = mongoose;
-//mongoose.connect('mongodb+srv://<usuario>:<contraseña>@<cluster>.mongodb.net/<base-de-datos>?retryWrites=true&w=majority', {
+//mongoose.connect(''mongodb+srv://<usuario>:<contraseña>@<cluster>.mongodb.net/<base-de-datos>?retryWrites=true&w=majority'', {
